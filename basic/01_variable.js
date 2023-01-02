fc7fc591ddaac6cb0f403b6178dccfd53bd79d7c
sophia6070 <sophia892@naver.com>
2023-01-02 15:28:59 +0900

    1234

---
+++ basic/01_variable.js
@@ -0,0 +1,57 @@
+// variable 변수
+// var, let, const
+// scope-> 블럭 {...}
+
+// 1. var
+// 읽기, 쓰기 가능
+// 블럭을 무시한다.
+
+{
+    var user = 'Sophia'
+    user = 'Alice'
+    console.log(user)
+}
+
+console.log(user)
+
+// 2.let
+// 읽기, 쓰기 가능
+
+{
+    let title = 'html'
+    console.log(title)
+
+    title = 'css'
+    console.log(title)
+}
+
+// console.log(title)
+
+// 3.const
+// 상수 - 읽기만 가능, 변하지 않는다.
+
+{
+    const subject = '야인시대'
+    console.log(subject)
+
+    // subject = '배트맨'
+}
+
+// 4.변수의 데이타 타입
+// 원시데이타(primitive)Number, String, Boolean, null, undefined
+
+// object : 싱글데이타를 묶어놓은 데이타
+// const myclass = {
+//     subject: 'html',
+//     data: '2022-12-13',
+// }
+
+// function
+// const hahaha = function () {
+//     console.log('hahahah~~~~')
+// }
+
+// number
+const num = 100
+const width = 10.5
+console.log(`값은 ${num}이고, 타입은 ${typeof num}입니다.`)
