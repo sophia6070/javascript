968ba612901e0275c660036080107731342277ba
sophia6070 <sophia892@naver.com>
2023-01-12 14:06:25 +0900

    js

--- tutorial/08.email/app.js
+++ tutorial/08.email/app.js
@@ -2,11 +2,26 @@ const dimm = document.querySelector('.dimm')
 const modal = document.querySelector('.modal')
 const closeBtn = document.querySelector('.close')
 const inputs = document.querySelectorAll('input')
+
+const userName = document.getElementById('userName')
+const email = document.getElementById('email')
+const message = document.getElementById('message')
+
 window.onload = function () {
     document
         .getElementById('contact-form')
         .addEventListener('submit', function (event) {
             event.preventDefault()
+
+            if (
+                userName.value == '' ||
+                email.value == '' ||
+                message.value == ''
+            ) {
+                alert('이름, 이메일, 내용을 확인하세요')
+                return
+            }
+
             dimm.classList.add('active')
             // generate a five digit number for the contact_number variable
             this.contact_number.value = (Math.random() * 100000) | 0
@@ -27,5 +42,6 @@ window.onload = function () {
         dimm.classList.remove('active')
 
         inputs.forEach((item) => (item.value = ''))
+        message.value = ''
     })
 }
