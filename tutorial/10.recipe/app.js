0cbd92225f12e2eac07dcf879f0bdf591b04e7fc
sophia6070 <sophia892@naver.com>
2023-01-13 15:32:05 +0900

    recipe

--- tutorial/10.recipe/app.js
+++ tutorial/10.recipe/app.js
@@ -8,12 +8,50 @@ const foodList = document.querySelector('.food_list')
 const APP_ID = '5470b548'
 const APP_KEY = '5a73909af775cd55cec304b31b033758'
 
-const baseUrl = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+const initQuery = 'chicken'
+
+function paintRecipe(items) {
+  console.log(items)
+
+  let foods = ''
+
+  items.map((item) => {
+    foods += ` <div class="food">
+          <div class="food_img"><img src="${item.recipe.image}" alt="" /></div>
+          <div class="food_info">
+            <div class="food__title">
+              <h3>${item.recipe.label}</h3>
+              <a href="${item.recipe.url}" target="_blank" class="btn">View recipe</a>
+            </div>
+            <p class="food_extra">Calories : ${item.recipe.calories}</p>
+            <p class="food_extra">Diet Lables : ${item.recipe.dietLabels}</p>
+          </div>
+          </div>`
+  })
+
+  console.log(foods)
+
+  foodList.innerHTML = foods
+}
+
+async function getRecipe(query) {
+  const baseUrl = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=20`
+
+  const response = await fetch(baseUrl)
+  const data = await response.json()
+
+  paintRecipe(data.hits)
+}
 
 function init() {
-  console.log(form)
-  console.log(input)
-  console.log(foodList)
+  getRecipe(initQuery)
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    const query = input.value
+    getRecipe(query)
+    input.value = ''
+  })
 }
 
 init()
